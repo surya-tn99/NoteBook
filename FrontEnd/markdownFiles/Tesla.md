a446708019e7e14042d91a42b70d9f25482b72d1
ALONE-VIBER <kuttypulisurya22@gmail.com>
2025-07-12 06:09:38 +0530

    updated batchFile

--- FrontEnd/markdownFiles/Tesla.md
+++ FrontEnd/markdownFiles/Tesla.md
@@ -1,5 +1,4 @@
-## my tesla 
-
+### _*T*esla_ 
 ![image][def]
 
 [def]: https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-blue-659ebaabd4e71.jpg?crop=1xw:1xh;center,top&resize=980:*
--- runMe.bat
+++ runMe.bat
@@ -1,17 +1,16 @@
 @echo off
-title Markdown Note App
+title NoteBook
+
+REM checking requirements
+pip install -r requirements.txt
 
 echo Starting Flask server on port 1234...
-REM Change to backend folder and run Flask server
+REM run Flask server
 start cmd /k "cd /d %~dp0backend && python server.py"
 
-echo Starting static HTML server on port 5000...
-REM Change to frontend folder and run static server
+echo Starting static HTML server on port 5678...
+REM run static server
 start cmd /k "cd /d %~dp0frontend && python -m http.server 5678"
 
-timeout /t 2 > nul
-
-echo Opening viewNotes.html in browser...
+echo Opening NoteBook in browser...
 start http://127.0.0.1:5678
-
-echo Servers are running. Close this window if needed.
